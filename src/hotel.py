4def57267b4925ef719227bb7b6fa39ffef35ef9
Beata Szeląg <b.z.szelag@gmail.com>
2018-12-10 23:22:26 +0100

    Update hotel.py

--- src/hotel.py
+++ src/hotel.py
@@ -1,7 +1,11 @@
 class Hotel(object):
-
     def __init__(self, name, rate, reviews, url):
         self.name = name
         self.rate = rate
         self.url = url
         self.reviews = reviews
+
+        
+        
+        
+        
